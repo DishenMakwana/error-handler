ad479a2c1e6b50dbaefa7a4280b5486fb81e9e2e
DISHEN <dishenmakwana.dm@gmail.com>
2022-07-10 11:46:20 +0530

    ADD: catchAsync

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,4 @@ export * from './middlewares/current-user';
 export * from './middlewares/error-handler';
 export * from './middlewares/require-auth';
 export * from './middlewares/validate-request';
+export * from './middlewares/catch.async';
