6b6642a70518aaadea96859d803b9ef63d0e501b
DISHEN <dishenmakwana.dm@gmail.com>
2022-07-15 18:38:25 +0530

    ADD: events for ticket nats service

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,9 @@ export * from './middlewares/error-handler';
 export * from './middlewares/require-auth';
 export * from './middlewares/validate-request';
 export * from './middlewares/catch.async';
+
+export * from './events/base-listener';
+export * from './events/base-publisher';
+export * from './events/ticket-created-event';
+export * from './events/ticket-updated-event';
+export * from './events/subjects';
