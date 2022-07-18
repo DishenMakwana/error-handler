292433eb30cb85d3e819807ee70964fa040c9a77
DISHEN <dishenmakwana.dm@gmail.com>
2022-07-18 14:23:13 +0530

    ADD: events for order service

--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,12 @@ export * from './middlewares/catch.async';
 
 export * from './events/base-listener';
 export * from './events/base-publisher';
-export * from './events/ticket-created-event';
-export * from './events/ticket-updated-event';
 export * from './events/subjects';
 
+export * from './events/ticket-created-event';
+export * from './events/ticket-updated-event';
+
 export * from './events/types/order-status';
+
+export * from './events/order-created-event';
+export * from './events/order-cancelled-event';
