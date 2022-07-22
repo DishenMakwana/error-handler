ac2302efe1d1063d61cc96ae5e4411f3f9bb2937
DISHEN <dishenmakwana.dm@gmail.com>
2022-07-22 16:43:17 +0530

    CHG: in common module

--- src/errors/not-authorized-error.ts
+++ src/errors/not-authorized-error.ts
@@ -4,7 +4,8 @@ export class NotAuthorizedError extends CustomError {
   statusCode = 401;
 
   constructor() {
-    super('Not authorized');
+    super('Not Authorized');
+
     Object.setPrototypeOf(this, NotAuthorizedError.prototype);
   }
 
